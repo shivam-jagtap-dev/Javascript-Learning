783b8ac6943941721b6cae56f4366146fe38c5e9
shivamjagtap58-cloud <shivamjagtap58@gmail.com>
2026-08-27 22:00:33 +0530

    Add Practice set 7(Functions

---
+++ Practice7/prac7.js
@@ -0,0 +1,29 @@
+
+//Question 1 
+//Create a function usingg the "function" keyword that takes a String as a argument & returns the number of vowels in the string.
+function countVowels(str) {
+    let count = 0;
+    for (const char of str) {
+        if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+            count++;
+        }
+    }
+
+    console.log(`There are ${count} in ${str} `);
+    return count;
+}
+
+//Question 2
+//Create a arrow function to perform same task
+
+const countVow = (str) => {
+    let count = 0;
+    for (const char of str) {
+        if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+            count++;
+        }
+    }
+
+    console.log(`There are ${count} in ${str} `);
+    return count;
+}
